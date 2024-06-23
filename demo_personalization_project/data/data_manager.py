6717ec5709f8d06c1f2eebdb725d529e97cbb619
Nijat Zeynalov <31247506+NijatZeynalov@users.noreply.github.com>
2024-06-23 22:50:10 +0400

    Delete demo_personalization_project directory

--- demo_personalization_project/data/data_manager.py
+++
@@ -1,8 +0,0 @@
-from app.utils import load_user_data, save_user_data
-
-def get_all_user_data():
-    return load_user_data()
-
-def get_user_profile(user_id):
-    user_data = load_user_data()
-    return user_data.get(user_id, {})
